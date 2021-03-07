6d71f71197afba725db452c471594d9d26e45015
IsaelJd <isaeldjunior@gmail.com>
2021-03-07 20:26:37 -0300

    add um novo if

--- test2.py
+++ test2.py
@@ -2,5 +2,7 @@ nome = str(input('Digite seu nome: '))
 print(f'Olá, {nome}!')
 if nome == 'Caio':
     print('Que lindo nome você tem.')
+elif nome == 'Joana':
+    print('Que nome diferente')
 else:
     print('Nome normal.')
