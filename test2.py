40b5e5d1c3d0fc8d8454c83bb632f93906f01d20
IsaelJd <isaeldjunior@gmail.com>
2021-03-07 20:39:29 -0300

    add nomes nos if

--- test2.py
+++ test2.py
@@ -1,8 +1,8 @@
-nome = str(input('Digite seu nome: '))
+nome = str(input('Digite seu nome: ')).strip().title()
 print(f'Olá, {nome}!')
-if nome == 'Caio':
+if nome == 'Caio' or 'Monica' or 'Pedro' or 'Isael':
     print('Que lindo nome você tem.')
-elif nome == 'Joana':
+elif nome == 'Joana' or 'Kaline' or 'Kailanne':
     print('Que nome diferente')
 else:
     print('Nome normal.')
